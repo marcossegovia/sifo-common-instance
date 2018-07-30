0ff54f4a96079046ec9e5310bd56b844519a3c6e
Marcos Segovia <velozmarkdrea@gmail.com>
2018-07-30 11:47:41 +0200

    Setting twig default config array values

--- config/views.config.php
+++ config/views.config.php
@@ -16,3 +16,13 @@ $config['smarty'] = array(
 	// Set this to false to avoid auto escape html strings.
 	'escape_html' => false,
 );
+
+$config['twig'] = array(
+	'custom_plugins_dir' => null, // Used to custom the default twig plugins directory.
+
+	// Set this to false to avoid magical parsing of literal blocks without the {literal} tags.
+	'auto_literal' => false,
+
+	// Set this to false to avoid auto escape html strings.
+	'escape_html' => false,
+);
